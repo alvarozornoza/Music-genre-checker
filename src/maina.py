2250841556000489c225302ee66214fde94157b6
alvarozornoza <azuzornoz@gmail.com>
2018-05-09 02:57:00 +0200

    Adding confusion matrix

--- src/maina.py
+++ src/maina.py
@@ -16,6 +16,11 @@ from config import sliceSize
 
 #from songToData import createSlicesFromAudio
 
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("mode", help="Trains or tests the CNN", nargs='+', choices=["train","test","slice"])
@@ -75,6 +80,62 @@ if "test" in args.mode:
 	print("[+] Test accuracy: {} ".format(testAccuracy))
 
 
+	evali= model.evaluate(test_X,test_y)[0]
+	print("Accuracy of the model is :", evali)
+	labels = model.predict_label(test_X)
+	print("The predicted labels are :",labels)
+	prediction = model.predict(test_X)
+	print("The predicted probabilities are :", prediction)
 
+	# Compute confusion matrix
+	cnf_matrix = confusion_matrix(labels, prediction)
+	np.set_printoptions(precision=2)
+
+	# Plot non-normalized confusion matrix
+	plt.figure()
+	plot_confusion_matrix(cnf_matrix, classes=class_names,
+						title='Confusion matrix, without normalization')
+
+	# Plot normalized confusion matrix
+	plt.figure()
+	plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+						title='Normalized confusion matrix')
+
+	plt.show()
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 
